5c72f036ceb1410c87824789eef48dd8a847ba7f
Naveen kumar <134396866+N4V33NM@users.noreply.github.com>
2025-02-13 21:43:35 +0530

    Delete AndroidKeylogger directory

--- AndroidKeylogger/app/src/main/java/com/example/callvoicerecorder/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.example.callvoicerecorder;
-
-public class Constants {
-    public static final String TELEGRAM_BOT_TOKEN = "8178078713:AAGOSCn4KEuvXC64xXhDrZjwQZmIy33gfaI";
-    public static final String TELEGRAM_CHAT_ID = "YOUR_CHAT_ID";
-}
